59c2dee63661abc5858e086d80b7799fb9efa81f
Crynowecer0 <crynowecer11@gmail.com>
2023-06-12 10:24:07 -0400

    complete findList.js

---
+++ findList.js
@@ -0,0 +1,20 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+const linkedListFind = (head, target) => {
+
+  let curr = head
+
+  while (curr) {
+   if (curr.val === target) {
+     return true
+   }
+    curr = curr.next
+  }
+
+  return false
+};
